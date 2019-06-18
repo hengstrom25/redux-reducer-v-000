3fc413d10fbbbfa4e051038eeb915c78538829fb
Heidi Engstrom <hengstrom25@gmail.com>
2019-06-18 19:44:10 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,5 +1,9 @@
 export function manageFriends(state, action){
+<<<<<<< HEAD
   switch(action.type) {
+=======
+  switch action.type {
+>>>>>>> 516541a93853d974f47062992c7b16c6d859e4bd
     case "ADD_FRIEND":
         return Object.assign({}, state, { friends: [...state.friends, action.friend] })
     case 'REMOVE_FRIEND':
